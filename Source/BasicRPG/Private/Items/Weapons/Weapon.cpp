e557023cb44061b4a0265fb15ae02a1f7d816e97
Seobok <Seobok@users.noreply.github.com>
2024-02-09 16:28:57 +0900

    [Update] Attach Item
    [Update] Attach Item

--- Source/BasicRPG/Private/Items/Weapons/Weapon.cpp
+++ Source/BasicRPG/Private/Items/Weapons/Weapon.cpp
@@ -3,9 +3,18 @@
 
 #include "Items/Weapons/Weapon.h"
 
+#include "Characters/BasicCharacter.h"
+
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	ABasicCharacter* BasicCharacter = Cast<ABasicCharacter>(OtherActor);
+	if (BasicCharacter)
+	{
+		FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
+		ItemMesh->AttachToComponent(BasicCharacter->GetMesh(), TransformRules, FName("RightHandSocket"));
+	}
 }
 
 void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
